3563a76fdc2f572d847b0ccc833036317951d020
Sami Koskivaara <sami.koskivaara@vr.fi>
2022-09-13 13:01:11 +0300

    add target state post

--- main.py
+++ main.py
@@ -6,27 +6,32 @@ from os import environ
 state_hash = hashlib.md5()
 
 
-def get_workspace_ids(org_name, base_url, headers):
-    ws_api_endpoint = f"/organizations/{org_name}/workspaces"
-    url = base_url + ws_api_endpoint
+def get_headers(token):
+    return {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
 
-    ws_data = requests.get(
+
+def get_workspace_ids(org_name, base_url, headers):
+    api_endpoint = f"/organizations/{org_name}/workspaces"
+    url = base_url + api_endpoint
+
+    data = requests.get(
         url, params=None, headers=headers).json()["data"]
-    return {ws["attributes"]["name"]: ws["id"] for ws in ws_data}
+    return {ws["attributes"]["name"]: ws["id"] for ws in data}
 
 
 def get_state_payload(workspace_id, base_url, headers):
-    ws_state_api_endpoint = f"/workspaces/{workspace_id}/current-state-version"
+    api_endpoint = f"/workspaces/{workspace_id}/current-state-version"
+    url = base_url + api_endpoint
 
-    ws_url = base_url + ws_state_api_endpoint
+    res = requests.get(url, params=None, headers=headers)
 
-    ws_res = requests.get(ws_url, params=None, headers=headers)
-    print(ws_res.status_code)
-
-    if ws_res.status_code != 200:
+    if res.status_code != 200:
         return
 
-    state_url = ws_res.json(
+    state_url = res.json(
     )["data"]["attributes"]["hosted-state-download-url"]
 
     state_res = requests.get(
@@ -54,24 +59,35 @@ def get_state_payload(workspace_id, base_url, headers):
     }
 
 
+def post_new_state(workspace_id, payload, base_url, headers):
+    api_endpoint = f"/workspaces/{workspace_id}/state-versions"
+    url = base_url + api_endpoint
+    res = requests.post(url, json=payload, headers=headers)
+    print('post new state status code:', res.status_code)
+
+
 def main():
-    source_workspace_id = environ["SOURCE_WS_ID"]
+    # $ENV:TARGET_ORG_NAME = ""
     source_org_name = environ["SOURCE_ORG_NAME"]
-    source_token = environ["SOURCE_TOKEN"]
-
-    headers = {
-        "Authorization": f"Bearer {source_token}",
-        "Content-Type": "application/json"
-    }
-
+    target_org_name = environ["TARGET_ORG_NAME"]
+    tfe_token = environ["TFE_TOKEN"]
     base_url = "https://app.terraform.io/api/v2"
 
-    source_ws_ids = get_workspace_ids(source_org_name, base_url, headers)
+    headers = get_headers(tfe_token)
 
-    for ws_name, ws_id in source_ws_ids.items():
-        print(ws_name)
-        d = get_state_payload(ws_id, base_url, headers)
-        print(d)
+    source_ws_ids = get_workspace_ids(
+        source_org_name, base_url, headers)
+
+    target_ws_ids = get_workspace_ids(
+        target_org_name, base_url, headers)
+
+    for source_ws_name, source_ws_id in source_ws_ids.items():
+        print('source ws name:', source_ws_name)
+        state_data = get_state_payload(source_ws_id, base_url, headers)
+        if state_data and source_ws_name in target_ws_ids:
+            target_ws_id = target_ws_ids[source_ws_name]
+            print('target ws id:', target_ws_id)
+            post_new_state(target_ws_id, state_data, base_url, headers)
 
 
 if __name__ == "__main__":
