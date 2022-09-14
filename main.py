42cc654029b2ced7bdb538fb87768789c72d3f60
Sami Koskivaara <sami.koskivaara@vr.fi>
2022-09-14 14:48:52 +0300

    add csv log

--- main.py
+++ main.py
@@ -2,10 +2,39 @@ import requests
 import hashlib
 import base64
 from os import environ
+import csv
+from dataclasses import dataclass
 
 from colorama import init, Fore, Back, Style
 
 init(autoreset=True)
+
+
+@dataclass
+class WorkspaceMigration:
+    """Class for migration state."""
+    source_org: str
+    target_org: str
+    workspace_name: str
+    source_ws_id: str
+    target_ws_id: str
+    info: str
+    state: str
+    migrated: bool
+
+    @staticmethod
+    def get_params():
+        return list(WorkspaceMigration.__annotations__.keys())
+
+
+def out_csv(data, file_name="migration.csv"):
+    fieldnames = WorkspaceMigration.get_params()
+    with open(file_name, 'w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for item in data:
+            writer.writerow(item.__dict__)
 
 
 def get_headers(token):
@@ -21,7 +50,7 @@ def get_workspace_ids(org_name, base_url, headers):
 
     data = requests.get(
         url, params=None, headers=headers).json()["data"]
-    return {ws["attributes"]["name"]: ws["id"] for ws in data}
+    return {ws["attributes"]["name"]: {"id": ws["id"], "locked": ws["attributes"]["locked"]} for ws in data}
 
 
 def get_state_payload(workspace_id, base_url, headers):
@@ -66,7 +95,7 @@ def get_state_payload(workspace_id, base_url, headers):
 def post_new_state(workspace_id, payload, base_url, headers):
     api_endpoint = f"/workspaces/{workspace_id}/state-versions"
     url = base_url + api_endpoint
-    res = requests.post(url, json=payload, headers=headers)
+    return requests.post(url, json=payload, headers=headers)
 
 
 def lock_workspace(workspace_id, base_url, headers, lock_type="lock"):
@@ -76,7 +105,9 @@ def lock_workspace(workspace_id, base_url, headers, lock_type="lock"):
 
 
 def main():
+    # $ENV:SOURCE_ORG_NAME = ""
     # $ENV:TARGET_ORG_NAME = ""
+    # $ENV:TFE_TOKEN = ""
     source_org_name = environ["SOURCE_ORG_NAME"]
     target_org_name = environ["TARGET_ORG_NAME"]
     tfe_token = environ["TFE_TOKEN"]
@@ -84,34 +115,81 @@ def main():
 
     headers = get_headers(tfe_token)
 
-    source_ws_ids = get_workspace_ids(
+    source_workspaces = get_workspace_ids(
         source_org_name, base_url, headers)
 
-    target_ws_ids = get_workspace_ids(
+    target_workspaces = get_workspace_ids(
         target_org_name, base_url, headers)
 
-    for source_ws_name, source_ws_id in source_ws_ids.items():
+    print('Source organization:', Style.BRIGHT +
+          Back.GREEN + Fore.BLACK + source_org_name)
+    print('Target organization:', Style.BRIGHT +
+          Back.BLUE + Fore.BLACK + target_org_name)
+
+    data = []
+
+    for source_ws_name, source_ws in source_workspaces.items():
+        target_ws_id = "NA"
+        info = "Migration not started"
+        state = "FAILED"
+        migrated = False
+
+        ws_migration = WorkspaceMigration(
+            source_org_name,
+            target_org_name,
+            source_ws_name,
+            source_ws["id"],
+            target_ws_id,
+            info,
+            state,
+            migrated
+        )
+
         print("\n")
         print(Style.BRIGHT + Back.BLUE + Fore.BLACK + source_ws_name)
         state_data = None
 
-        if source_ws_name in target_ws_ids:
-            state_data = get_state_payload(source_ws_id, base_url, headers)
+        if source_ws_name in target_workspaces:
+            state_data = get_state_payload(source_ws["id"], base_url, headers)
         else:
+            info = f"Source workspace name not found from target workspaces"
+
             print(Style.BRIGHT + Back.RED + Fore.BLACK +
                   "STATE MIGRATION FAIL")
-            print("Source workspace name:", source_ws_name,
-                  "not found from target workspaces!")
+            print(info)
+            ws_migration.info = info
+            data.append(ws_migration)
+            continue
 
         if state_data:
-            target_ws_id = target_ws_ids[source_ws_name]
+            target_ws = target_workspaces[source_ws_name]
+            target_ws_id = target_ws["id"]
+            ws_migration.target_ws_id = target_ws_id
             print(Style.BRIGHT + Back.GREEN + Fore.BLACK +
                   "STATE MIGRATION SUCCESS")
             print('Target workstation ID', target_ws_id)
 
-            lock_workspace(target_ws_id, base_url, headers)
+            if not target_ws["locked"]:
+                lock_workspace(target_ws_id, base_url, headers)
+
             post_new_state(target_ws_id, state_data, base_url, headers)
-            lock_workspace(target_ws_id, base_url, headers, "unlock")
+            ws_migration.info = "Migration OK"
+            ws_migration.state = "MIGRATED"
+            ws_migration.migrated = True
+
+            if not target_ws["locked"]:
+                lock_workspace(target_ws_id, base_url, headers, "unlock")
+        else:
+            info = f"Source workspace does not have any state"
+            print(Style.BRIGHT + Back.YELLOW + Fore.BLACK +
+                  "STATE MIGRATION SKIPPED")
+            print(info)
+            ws_migration.info = info
+            ws_migration.state = "SKIPPED"
+            ws_migration.migrated = False
+        data.append(ws_migration)
+
+    out_csv(data)
 
 
 if __name__ == "__main__":
